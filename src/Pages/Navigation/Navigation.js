1848c272b27386eb7a3f44395dcb7100667ab395
sabbir0029 <mdsabbirhossain0029@gmail.com>
2022-01-03 11:20:24 +0600

    some text

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './Navigation.css';
+import UseAuth from '../Hooks/UseAuth';
 
 const Navigation = () => {
+    const {user,logOut} = UseAuth();
     return (
-        <div className='nav-style'>
-            <div className='logo'>
-            <img src="https://pawsofcoronado.org/wp-content/uploads/2016/01/cropped-favicon.png" alt="" />
-            
-            </div>
-            
-    <Nav.Link as={Link} to="/home">Home</Nav.Link>
-    <Nav.Link as={Link} to="/login">Login</Nav.Link>
-    <Nav.Link as={Link} to="/aboutus">About Us</Nav.Link>
-    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-   
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
+          <Container>
+          <Navbar.Brand as={Link} to="/home" className='text-dark'><span className='text-danger fw-bold'>PET.</span> SHOP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/home" className='text-dark'>Home</Nav.Link>
+              <Nav.Link as={Link} to="/Services" className='text-dark'>Services</Nav.Link>
+              <Nav.Link as={Link} to="/MyOders" className='text-dark'>My Oders</Nav.Link>
+            </Nav>
+            <Nav>
+              { user?.email? 
+              <Button onClick={logOut}>logOut</Button>
+              :
+                <Nav.Link as={Link} to="/login" className='text-dark'>Login</Nav.Link>
+              }
+              <Nav.Link as={Link} to="/register" className='text-dark'>Register</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes" className='text-dark'>
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          </Container>
+       </Navbar>
         </div>
     );
 };

1ae9eebb5c25bbda578431054b77699059aa1e00
tonny29 <zannatulnayemtonny@gmail.com>
2022-01-04 13:59:17 +0600

    add about us

--- src/Pages/Aboutus/Aboutus.js
+++ src/Pages/Aboutus/Aboutus.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import catHand from '../../petImage/hero_image_12.png';
+import winone from '../../petImage/winone.jpg';
+import wintwo from '../../petImage/wintwo.jpg';
+import winthree from '../../petImage/winthree.jpg';
+import winfour from '../../petImage/winfour.jpg';
 import './Aboutus.css';
 
 const Aboutus = () => {
@@ -8,7 +12,7 @@ const Aboutus = () => {
         <div className='allAbout'>
             <img src={catHand} alt="" />
             <div className='aboutus'>
-                <h1>ABOUT US</h1>
+                <h1> <span style={{color:'brown'}}>ABOUT</span> US</h1>
                 <p>From the time our friends sniff their way through the door until they wag their <br /> tails out in the afternoon, we cater to their nature.</p>
                 <div className='row'>
                     <Col lg={3}>
@@ -41,18 +45,60 @@ const Aboutus = () => {
                     </Col>
                 </div>
             </div>
+            <br /><br />
             <div>
-                <h1>ABOUT OUR FOOD QUALITY</h1>
-                <p>Pet food quality is difficult to define but is easily measured by the ultimate judge, the pets.Consumers often evaluate quality on the basis of ingredients, guaranteed analysis, palatability, physical form and appearance of ration, etc. </p>
+                <h1>ABOUT <span style={{color:'brown'}}>OUR FOOD</span> QUALITY</h1>
+                <p>Pet food quality is difficult to define but is easily measured by the ultimate judge, the pets.Consumers often evaluate quality on the basis of <br /> ingredients, guaranteed analysis, palatability, physical form and appearance of ration, etc. </p>
                 <div className='row'>
-                    <Col lg={3}>one part to learn</Col>
-                    <Col lg={3}>one part to learn</Col>
-                    <Col lg={3}>one part to learn</Col>
-                    <Col lg={3}>one part to learn</Col>
+                    <Col lg={3}>
+                        <div className='card-style'>
+                        <img src="https://i.dlpng.com/static/png/7000069_preview.png" alt="" />
+                        </div>
+                    </Col>
+                    <Col lg={3}>
+                    <div className='card-style'>
+                    <img src="https://www.justrightpetfood.com/media/wysiwyg/bowl.png" alt="" />
+                    </div>
+                    </Col>
+                    <Col lg={3}>
+                    <div className='card-style'>
+                    <img src="https://freshpet.com/wp-content/uploads/2013/10/NF_DOG_WM_GrainFreeChickenwithCarrotsSpinach_5.png" alt="" />
+                    </div>
+                    </Col>
+                    <Col lg={3}>
+                    <div className='card-style'>
+                    <img src="https://image.freepik.com/free-photo/dry-cat-food-bowl-isolated-white-background_102618-2009.jpg" alt="" />
+                    </div>
+                    </Col>
                 </div>
             </div>
+            <br /><br />
             <div>
-                <h1>Tjis is third part</h1>
+                <h1>An <span style={{color:'brown'}}>award-winning</span> pet food</h1>
+                <p>Our whole-prey raw inspired recipes, chews and rewards are handcrafted
+in small <br /> batches using finest ingredients</p>
+                <div className='row'>
+                    <Col lg={3}>
+                        <div className='win-style'>
+                        <img src={winone} alt="" />
+                        </div>
+                    </Col>
+                    <Col lg={3}>
+                    <div className='win-style'>
+                    <img src={wintwo} alt="" />
+                    </div>
+                    </Col>
+                    <Col lg={3}>
+                    <div className='win-style'>
+                    <img src={winthree} alt="" />
+                    </div>
+                    </Col>
+                    <Col lg={3}>
+                    <div className='win-style'>
+                    <img src={winfour} alt="" />
+                    </div>
+                    </Col>
+                </div>
             </div>
         </div>
     );

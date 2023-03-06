490e00b88fc0ee5aae2ff820ff7b42ed9907ff36
tonny29 <zannatulnayemtonny@gmail.com>
2023-03-06 15:28:41 +0600

    update detail page

--- src/Pages/FakeDataForDetail/FakeData.js
+++ src/Pages/FakeDataForDetail/FakeData.js
@@ -1,58 +1,57 @@
 const fakedata=[
-
-    {
-      "id": 1,
-      "name": "Selmon Fish",
-      "totalCost": 1000,
-      "image":"https://media.istockphoto.com/photos/dry-pet-food-picture-id1131841641?k=20&m=1131841641&s=612x612&w=0&h=T4YEBAocjxLg0Abl_an1pYbM5zyUFuXHTbNRVF4Czag="
-    },
-    {
-      "id": 2,
-      "name": "Chicken",
-      "totalCost": 2000,
-      "image":"https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fdogoday.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F07%2F1229904411.jpeg"
-    },
-    {
-      "id": 3,
-      "name": "Air dried",
-      "totalCost": 1050,
-      "image":"https://static01.nyt.com/images/2019/07/01/us/29xp-dogfood/29xp-dogfood-videoSixteenByNineJumbo1600.jpg"
-    },
-    {
-      "id": 4,
-      "name": "Vegatble",
-      "totalCost": 500,
-      "image":"https://www.petfoodindustry.com/ext/resources/Images-by-month-year/20_08/Pet-food-bulk-close-up.jpg"
-    },
-    {
-      "id": 5,
-      "name": "chia seeds",
-      "totalCost": 2200,
-      "image":"https://soperth.com.au/wp-content/uploads/2022/05/LYKA_RooRecipe_200921_YMund-1254-1-HR-1024x683.jpg"
-    },
-    {
-      "id": 6,
-      "name": "Sweet potato",
-      "totalCost": 1000,
-      "image":"https://static.aeroxinjector.com/_674x330_/producten/pet-food/pet_food_industries_1599651302.jpg"
-    },
-    {
-      "id": 7,
-      "name": "Pumpkin",
-      "totalCost": 800,
-      "image":"https://www.kazior5.com/wp-content/uploads/2019/05/pet-food.jpg"
-    },
-    {
-      "id": 8,
-      "name": "Box Food",
-      "totalCost": 1200,
-      "image":"https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fdogoday.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F07%2F1229904411.jpeg"
-    },
-    {
-      "id": 9,
-      "name": "Fish",
-      "totalCost": 500,
-      "image":"https://brightsideanimals.org/wp-content/uploads/2019/01/food-bank.jpg"
-    }
+  {
+    "id": 1,
+    "name": "",
+    "totalCost": 1000,
+    "image":"https://soperth.com.au/wp-content/uploads/2022/05/LYKA_RooRecipe_200921_YMund-1254-1-HR-1024x683.jpg"
+  },
+  {
+    "id": 2,
+    "name": "",
+    "totalCost": 2000,
+    "image":"https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fdogoday.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F07%2F1229904411.jpeg"
+  },
+  {
+    "id": 3,
+    "name": "",
+    "totalCost": 1050,
+    "image":"https://static01.nyt.com/images/2019/07/01/us/29xp-dogfood/29xp-dogfood-videoSixteenByNineJumbo1600.jpg"
+  },
+  {
+    "id": 4,
+    "name": "",
+    "totalCost": 500,
+    "image":"https://target.scene7.com/is/image/Target/8wk3_Pets_Kindfull_PremiumCat-210715-1626385072300?wid=668&qlt=80&fmt=webp"
+  },
+  {
+    "id": 5,
+    "name": "",
+    "totalCost": 2200,
+    "image":"https://www.seniorcatwellness.com/wp-content/uploads/2020/05/what-are-the-best-vegetables-for-cats.jpg"
+  },
+  {
+    "id": 6,
+    "name": "",
+    "totalCost": 1000,
+    "image":"https://images.avana.asia/176597/images/x-large/988b25164d00f34b5f14dc52e588cad5d332c7e3"
+  },
+  {
+    "id": 7,
+    "name": "",
+    "totalCost": 800,
+    "image":"https://www.kazior5.com/wp-content/uploads/2019/05/pet-food.jpg"
+  },
+  {
+    "id": 8,
+    "name": "",
+    "totalCost": 1200,
+    "image":"https://www.purina.co.nz/sites/default/files/styles/ttt_image_510/public/2020-12/Can%20Cats%20Eat%20Dog%20Food2.jpg?itok=Sjq6j434"
+  },
+  {
+    "id": 9,
+    "name": "",
+    "totalCost": 500,
+    "image":"https://brightsideanimals.org/wp-content/uploads/2019/01/food-bank.jpg"
+  }
 ]
 export default fakedata;
--- src/Pages/FoodDetails/FoodDetails.js
+++ src/Pages/FoodDetails/FoodDetails.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import fakedata from '../FakeDataForDetail/FakeData';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import fakedata from "../FakeDataForDetail/FakeData";
+import './FoodDetails.css';
 
 const FoodDetails = () => {
-    const {detailId}=useParams();
-    const details=fakedata.find(data=>data.id===parseInt(detailId));
-    return (
-        <div>
-            <h1>this is detail page: {detailId}</h1>
-            <h3>Name: {details.name}</h3>
-
-            <img src={details.image} alt="" style={{
-                height: "400px", width: "400px",marginTop: "20px"
-            }} />
-        </div>
-    );
+  const { detailId } = useParams();
+  const details = fakedata.find((data) => data.id === parseInt(detailId));
+  return (
+    <div>
+      <h1>this is detail page: {detailId}</h1>
+      <Container>
+        <Row>
+          <Col xs={6}>
+            <div className="SelectImage">
+                <img src={details.image} alt=""/>
+            </div>
+            
+          </Col>
+          <Col xs={6}>
+            <div className="foodDetails">
+                <h2>Name Of Food: {details.name}</h2>
+                <p>This Food Have Below Protins:</p>
+                <ul>
+                    <li>Calsium</li>
+                    <li>Calsium</li>
+                    <li>Calsium</li>
+                </ul>
+            </div>
+          </Col>
+        </Row>
+        </Container>
+    </div>
+  );
 };
 
 export default FoodDetails;
